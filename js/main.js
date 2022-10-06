2f2220993190f37e817fcbb3930e7d7ef7b19fe7
alexGalvan0 <alex.galvan@student.awesomeinc.org>
2022-10-06 21:06:00 +0000

    added style

--- js/main.js
+++ js/main.js
@@ -1,89 +1,88 @@
 let model = {
-    winConditions : [
-        [0,1,2],
-        [0,3,6],
-        [0,4,8],
-        [3,4,5],
-        [1,4,7],
-        [2,4,6],
-        [6,7,8],
-        [2,5,8]
+    winConditions: [
+        [0, 1, 2],
+        [0, 3, 6],
+        [0, 4, 8],
+        [3, 4, 5],
+        [1, 4, 7],
+        [2, 4, 6],
+        [6, 7, 8],
+        [2, 5, 8]
     ],
-    
-   board : [
+
+    board: [
         '', '', '',
         '', '', '',
-        '','', ''
+        '', '', ''
     ],
     playerTurn: 'X',
-    
-    xPositions : [],
-    oPositions : []
+
+    xPositions: [],
+    oPositions: []
 }
 
-
-let view ={
-    render: function(){
+let view = {
+    render: function () {
         const body = document.body;
         body.classList.add('text-center')
 
         let container = view.createElements({
-            classes:['container','d-flex','justify-content-center','mt-5'],
-            parent:app
+            classes: ['container', 'd-flex', 'justify-content-center', 'mt-5'],
+            parent: app
         })
         let title = this.createElements({
-            type:'h1',
-            text:'TIC-TAC-TOE',
-            parent:app
+            type: 'h1',
+            text: 'TIC-TAC-TOE',
+            parent: app
         })
 
         let row = view.createElements({
-            classes:['row'],
-            parent:container
+            classes: ['row'],
+            parent: container
         })
 
         let col = view.createElements({
-            classes:['col'],
+            classes: ['col'],
             parent: row
         })
         let currentPlayer = view.createElements({
-            type:'h3',
-            text:`its player ${model.playerTurn}'s turn`,
-            parent:app
+            type: 'h3',
+            text: model.playerTurn,
+            parent: app
         })
 
-        for (let i=0; i<9; i++){
-           let tile = view.createElements({
-                type:'button',
-                id:i,
-                parent:col,
-                classes:['btns']
+        for (let i = 0; i < 9; i++) {
+            let tile = view.createElements({
+                type: 'button',
+                id: i,
+                parent: col,
+                classes: ['btns']
             })
-            
-            tile.addEventListener('click',() => {
+
+            tile.addEventListener('click', () => {
                 tile.innerHTML = model.playerTurn;
-                if(model.playerTurn == "X"){
+                if (model.playerTurn == "X") {
                     model.playerTurn = 'O'
                 } else {
-                    model.playerTurn ="X"
+                    model.playerTurn = "X"
                 }
                 tile.disabled = true
                 model.board[i] = tile.innerHTML
                 this.checkWinCondition()
-                currentPlayer.innerHTML = `its player ${model.playerTurn}'s turn` // add conditional
+                currentPlayer.innerHTML = model.playerTurn // add conditional
 
             })
         }
 
         let resetButton = view.createElements({
-            type:'button',
-            classes:['text-center','border','btn','btn-primary'],
-            text:'RESET',
-            parent:app
+            type: 'button',
+            classes: ['text-center', 'border', 'btn', 'btn-primary'],
+            text: 'RESET',
+            parent: app
         })
         resetButton.addEventListener('click', controller.reset)
     },
-     createElements: function({
+    createElements: function ({
         type = 'div',
         text = '',
         parent,
@@ -92,10 +91,10 @@ let view ={
         alt,
         src,
         setAttribute
-    
+
     } = {}) {
         let element = document.createElement(type)
-    
+
         if (classes == undefined) {
             ''
         } else {
@@ -112,57 +111,56 @@ let view ={
         return element
     },
 
-    checkWinCondition: function() {
+    checkWinCondition: function () {
         //check win condiition
-        model.board.forEach((item,i) => {
-            if(item == 'X'){
-                if(model.xPositions.includes(i)){
+        model.board.forEach((item, i) => {
+            if (item == 'X') {
+                if (model.xPositions.includes(i)) {
                     return
                 } else {
                     model.xPositions.push(i)
-            }
-            }else if(item == 'O'){
-                if(model.oPositions.includes(i)){
+                }
+            } else if (item == 'O') {
+                if (model.oPositions.includes(i)) {
                     return
                 } else {
                     model.oPositions.push(i)
-            }
-    
+                }
+
             }
         })
         let sortedX = model.xPositions.sort().toString()
         let sortedO = model.oPositions.sort().toString();
-    
-        model.winConditions.forEach((winCondition) => {
-            if(sortedX.includes(winCondition)){
-                model.playerTurn = 'X Wins'
-                setTimeout(controller.reset,2000)
 
-            } else if( sortedO.includes(winCondition)){
+        model.winConditions.forEach((winCondition) => {
+            if (sortedX.includes(winCondition)) {
+                model.playerTurn = 'X Wins'
+                setTimeout(controller.reset, 2000)
+
+            } else if (sortedO.includes(winCondition)) {
                 model.playerTurn = 'O wins'
-                setTimeout(controller.reset,2000)
-     
-               
-            } else if(! model.board.includes('')){
-                model.playerTurn = 'It is a tie'
-                setTimeout(controller.reset,2000)
+                setTimeout(controller.reset, 2000)
+
+
+            } else if (!model.board.includes('')) {
+                model.playerTurn = 'TIE!'
+                setTimeout(controller.reset, 2000)
             }
         })
-    
-        
+
+
     }
-    
+
 
 }
 
-//interaction
-let controller ={
-    init: function(){
 
+let controller = {
+    init: function () {
         view.render()
     },
 
-    reset: function(){
+    reset: function () {
         const body = document.body;
         body.classList.add('text-center')
         const app = document.getElementById('app')
@@ -170,31 +168,29 @@ let controller ={
 
         app.remove()
         view.createElements({
-            id:'app',
+            id: 'app',
             parent: body
         })
 
-       model.board = [
+        model.board = [
             '', '', '',
             '', '', '',
-            '','', ''
+            '', '', ''
         ]
         model.xPositions = [];
         model.oPositions = [];
         model.playerTurn = 'X'
         controller.init()
     },
-    
-    updatePlayer : function(){
-      if(model.playerTurn == 'X'){
-        model.playerTurn = 'O'
-      } else if  ( model.playerTurn == 'O'){
-        model.playerTurn = 'X'
-      }
+
+    updatePlayer: function () {
+        if (model.playerTurn == 'X') {
+            model.playerTurn = 'O'
+        } else if (model.playerTurn == 'O') {
+            model.playerTurn = 'X'
+        }
 
     }
 
 }
-controller.init()
-
-
+controller.init();
