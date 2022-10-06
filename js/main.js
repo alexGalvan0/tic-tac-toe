17c1a28a8e6ac52c9052b698a3143c4bbd0b1681
alexGalvan0 <alex.galvan@student.awesomeinc.org>
2022-10-06 17:30:00 +0000

    update

--- js/main.js
+++ js/main.js
@@ -1,6 +1,24 @@
-import { createElements } from './function.js';
-import { model } from './model.js';
-
+let model = {
+    winConditions : [
+        [0,1,2],
+        [0,3,6],
+        [0,4,8],
+        [3,4,5],
+        [1,4,7],
+        [2,4,6],
+        [6,7,8],
+        [2,5,8]
+    ],
+    
+   board : [
+        '', '', '',
+        '', '', '',
+        '','', ''
+    ],
+    playerTurn: 'X',
+    xPositions : [],
+    oPositions : []
+}
 
 //visible data
 let view ={
@@ -8,23 +26,23 @@ let view ={
         const body = document.body;
         body.classList.add('text-center')
 
-        let container = createElements({
+        let container = view.createElements({
             classes:['container','d-flex','justify-content-center'],
             parent:app
         })
 
-        let row = createElements({
+        let row = view.createElements({
             classes:['row'],
             parent:container
         })
 
-        let col = createElements({
+        let col = view.createElements({
             classes:['col'],
             parent: row
         })
 
         for (let i=0; i<9; i++){
-           let tile = createElements({
+           let tile = view.createElements({
                 type:'button',
                 id:i,
                 parent:col,
@@ -43,19 +61,47 @@ let view ={
 
             })
         }
-        let currentPlayer = createElements({
+        let currentPlayer = view.createElements({
             type:'h3',
             text:`It is player ${model.playerTurn}'s turn`,
             parent:app
         })
 
-        let resetButton = createElements({
+        let resetButton = view.createElements({
             type:'button',
             classes:['text-center','border'],
             text:'RESET',
             parent:app
         })
         resetButton.addEventListener('click', controller.reset)
+    },
+     createElements: function({
+        type = 'div',
+        text = '',
+        parent,
+        classes,
+        id = '',
+        alt,
+        src,
+        setAttribute
+    
+    } = {}) {
+        let element = document.createElement(type)
+    
+        if (classes == undefined) {
+            ''
+        } else {
+            classes.forEach(classAdded => {
+                element.classList.add(classAdded)
+            });
+        }
+        element.innerHTML = text;
+        element.id = id
+        element.alt = alt
+        element.src = src
+        element.setAttribute = setAttribute
+        parent.appendChild(element)
+        return element
     },
 
     checkWinCondition: function() {
@@ -112,7 +158,7 @@ let controller ={
         body.classList.add('text-center')
 
         app.remove()
-        createElements({
+        view.createElements({
             id:'app',
             parent: body
         })
